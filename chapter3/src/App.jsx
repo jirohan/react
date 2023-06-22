d972303e9620f53f5ea9394e27c0e2a2a8c1fdd8
Rohan Maharjan <hondazmr6@gmail.com>
2023-06-22 13:40:34 +0545

    cat api

---
+++ chapter3/src/App.jsx
@@ -0,0 +1,32 @@
+import { useEffect, useState } from "react";
+import "./App.css";
+import Axios from "axios";
+
+function App() {
+  const [catFact, setCatFact] = useState("");
+
+  const fetchCatFact = () => {
+    Axios.get("https://catfact.ninja/fact").then((res) => {
+      setCatFact(res.data.fact);
+    });
+  }
+
+  // fetch("https://catfact.ninja/fact")
+  // .then((res) => res.json())
+  // .then((data) => {
+  //   console.log(data)
+  // });
+
+  useEffect(() => {
+    fetchCatFact();
+  }, []);
+
+  return (
+    <div className="App">
+      <button onClick={fetchCatFact}>Generate Cat Fact</button>
+      <p>{catFact}</p>
+    </div>
+  );
+}
+
+export default App;
