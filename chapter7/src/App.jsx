e0b26a030d91d4341a063d3743bc7fcdccce9922
Rohan Maharjan <hondazmr6@gmail.com>
2023-06-22 17:26:55 +0545

    context api

---
+++ chapter7/src/App.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { Profile } from "./pages/Profile";
+import { Contact } from "./pages/Contact";
+import { Navbar } from "./Navbar";
+import { useState, createContext } from "react";
+
+export const AppContext = createContext();
+
+const App = () => {
+  const [username, setUsername] = useState("JyapuTech");
+
+  return (
+    <div className="App">
+      <AppContext.Provider value={{username, setUsername}}>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/profile"
+              element={
+                <Profile />
+              }
+            />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="*" element={<h1>Page Not Found</h1>} />
+          </Routes>
+        </Router>
+      </AppContext.Provider>
+    </div>
+  );
+};
+
+export default App;
