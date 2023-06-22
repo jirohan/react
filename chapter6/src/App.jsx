6ec1e9e12b7bfbd28b6853d53208a0edc000d880
Rohan Maharjan <hondazmr6@gmail.com>
2023-06-22 16:11:40 +0545

    router-dom

---
+++ chapter6/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./App.css"
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Home } from './pages/Home';
+import { Menu } from './pages/Menu';
+import { Contact } from './pages/Contact';
+import { Navbar } from './Navbar';
+
+const App = () => {
+  return (
+    <div className='App'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path = "/" element={<Home/>} />
+          <Route path = "/menu" element={<Menu/>} />
+          <Route path = "/contact" element={<Contact/>} />
+          <Route path = "*" element={<h1>Page Not Found</h1>} />
+        </Routes>
+      </Router>
+    </div>
+  )
+}
+
+export default App
