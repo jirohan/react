fc6a235c0e3a7ae3829f77c1bcb6befb1fae2ef0
Rohan Maharjan <hondazmr6@gmail.com>
2023-06-22 14:14:50 +0545

    age prediction

---
+++ chapter4/src/App.jsx
@@ -0,0 +1,31 @@
+import Axios from "axios";
+import { useState, useEffect } from "react";
+
+function App () {
+  const [name, setName] = useState("");
+  const [predictedAge, setPredictedAge] = useState(null);
+
+  const fetchData = () => {
+    Axios.get(`https://api.agify.io/?name=${name}`).then((res) => {
+  setPredictedAge(res.data);     
+    })
+  };
+
+  return (
+    <div className='App'>
+      <input 
+      type="text" 
+      placeholder="Ex. Ram.." 
+      onChange={(event) => {setName(event.target.value);
+      }}  
+      />
+      <button onClick={fetchData}>Predict age</button>
+
+      <h1>Name: {predictedAge?.name}</h1>
+      <h1>Predicted Age: {predictedAge?.age}</h1>
+      <h1>Count: {predictedAge?.count}</h1>
+    </div>
+  )
+}
+
+export default App
