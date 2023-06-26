6025f44cb99424e7ef8c48a57099e39e7b173d77
Rohan Maharjan <hondazmr6@gmail.com>
2023-06-26 13:48:47 +0545

    'basics-recall'

---
+++ React-MOSH/src/main.jsx
@@ -0,0 +1,29 @@
+//var -> function
+//let -> block
+//const -> block
+
+// function sayHello(){
+//   for (let i = 0; i < 5; i++){
+//     console.log(i);
+//   }
+//   console.log(i);
+// }
+
+// sayHello();
+
+// const x= 1;
+// x= 2;
+// console.log(x);
+
+//OBJECTS
+const person = {
+  name: 'Ram',
+  walk () {},
+  talk() {}
+  
+};
+
+person.talk();
+
+const targetMember = 'name';
+person[targetMember.value] = 'Shyayam'; 
