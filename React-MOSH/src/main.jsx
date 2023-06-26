de4e80d527fd082f11372dade155d3d40c3cd203
Rohan Maharjan <hondazmr6@gmail.com>
2023-06-26 14:44:01 +0545

    js basics

--- React-MOSH/src/main.jsx
+++ React-MOSH/src/main.jsx
@@ -16,14 +16,61 @@
 // console.log(x);
 
 //OBJECTS
-const person = {
-  name: 'Ram',
-  walk () {},
-  talk() {}
+// const person = {
+//   name: 'Ram',
+//   walk () {},
+//   talk() {}
   
+// };
+
+// person.talk();
+// person.name = '';
+
+// const targetMember = 'name';
+// person[targetMember.value] = 'Shyayam'; 
+
+//this
+// const person = {
+//   name: "Ram",
+//   walk() {
+//     console.log(this);
+//   }
+// };
+
+// person.walk();
+
+// const walk = person.walk.bind(person);
+// walk();
+
+//window object
+
+
+//Arrow Function
+//method 1
+// const square = (number) => {
+//   return number * number;
+// }
+// console.log(square(5));
+
+// //method 2
+// const square1 = number => number * number;
+// console.log(square1(57));
+
+// //example 1
+// const jobs = [
+//   { id: 1, isActive: true },
+//   { id: 2, isActive: true },
+//   { id: 3, isActive: false },
+// ];
+
+// const activejobs = jobs.filter(job => job.isActive);
+
+const person = {
+  talk() {
+    setTimeout(() => {
+      console.log("this", this);
+    }, 2000);
+  }
 };
 
 person.talk();
-
-const targetMember = 'name';
-person[targetMember.value] = 'Shyayam'; 
