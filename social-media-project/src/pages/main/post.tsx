34d90cec86443983a4da3f64094275a2795edfdb
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-10 17:57:18 +0545

    get docs

--- social-media-project/src/pages/main/post.tsx
+++ social-media-project/src/pages/main/post.tsx
@@ -1,4 +1,7 @@
+import { addDoc, getDocs, collection, query, where } from 'firebase/firestore';
 import {Post as IPost} from './main-page';
+import { auth, db } from '../../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
 
 interface Props {
     post: IPost;
@@ -6,5 +9,39 @@ interface Props {
 
 export const Post = (props: Props) => {
     const {post} = props;
-    return <div> </div>
+    const [user] = useAuthState(auth);
+
+    const likesRef = collection(db, "likes");
+
+    const likesDoc = query(likesRef, where("postId", "==", post.id));
+
+    const getLikes = async() => {
+        const data = await getDocs(likesDoc)
+        console.log(data.data.docs.map((doc) => ({...doc.data(), id:doc.id})));
+    }
+
+    const addLike = async() => {
+        await addDoc(likesRef, { 
+            userId: user?.uid,
+            postId: post.id 
+        });
+
+    };
+
+    return (
+        <div> 
+            <div className="title">
+                <h1>{post.title}</h1>
+            </div>
+            <div className='body'>
+                <p>{post.description}</p>
+            </div>
+
+            <div className='footer'>
+                <p>@{post.username}</p>
+                <button onClick={addLike}> &#128077; </button>
+                <p>Likes: {} </p>
+            </div>
+        </div>
+        )
 }
