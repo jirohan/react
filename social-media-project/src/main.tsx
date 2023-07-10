9f469a908979989516eb297ec181e5576574f30f
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-10 15:25:51 +0545

    main file

--- social-media-project/src/main.tsx
+++ social-media-project/src/main.tsx
@@ -1,5 +1,7 @@
-
+import React from 'react';
 import App from './App.tsx'
+import ReactDOM from 'react-dom/client';
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
