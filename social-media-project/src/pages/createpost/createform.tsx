f00c1e010d18cd89de8be9fa46394ef10304a534
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-10 16:42:04 +0545

    form completed

--- social-media-project/src/pages/createpost/createform.tsx
+++ social-media-project/src/pages/createpost/createform.tsx
@@ -1,25 +1,52 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { useForm } from'react-hook-form';
 import * as yup from "yup";
+import {addDoc, collection} from 'firebase/firestore';
+import {auth, db} from '../../config/firebase';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import {useNavigate} from 'react-router-dom'
+
+interface CreateFormData {
+    title: string;
+    description: string;
+}
 
 export const CreateForm = () => {
+
+    const [user] = useAuthState(auth);
+    const navigate = useNavigate();
+
     const schema = yup.object().shape({
         title: yup.string().required("You must add a title."),
         description: yup.string().required("You must add a description."),
     });
 
-    const { register, handleSubmit } = useForm({
+    const { 
+        register, 
+        handleSubmit, 
+        formState: {errors}, 
+    } = useForm<CreateFormData>({
         resolver: yupResolver(schema),
     });
 
-    const onCreatePost = (data: any) => {
-        console.log(data);
+    const postsRef = collection(db, "posts");
+
+    const onCreatePost = async(data: CreateFormData) => {
+        await addDoc(postsRef, {
+            ...data,
+            username: user?.displayName,
+            userId: user?.uid,
+        });
+
+        navigate("/");
     };
 
     return (
         <form action="" onSubmit={handleSubmit(onCreatePost)}>
             <input type="text" placeholder='Title...' {...register("title")}  />
+            <p style={{color: "red"}}> {errors.title?.message} </p>
             <textarea placeholder='Description...' {...register("description")} />
+            <p style={{color: "red"}}> {errors.description?.message} </p>
             <input type="submit"  />
         </form>
     )
