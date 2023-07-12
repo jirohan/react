867df25a04c8455ade08d0c9b7c1ced45ab714ca
Rohan Maharjan <hondazmr6@gmail.com>
2023-07-12 14:20:22 +0545

    handle click

--- React-Dave/tut0/src/Content.jsx
+++ React-Dave/tut0/src/Content.jsx
@@ -5,13 +5,26 @@ const Content = () => {
         const names = ['rohan', 'bob', 'kelvin'];
         const int = Math.floor(Math.random() * 3);
         return names[int];
-      }
+    }
+
+    const handleClick = () => {
+        console.log("You clicked it")
+    }
+    const handleClick2 = (name) => {
+        console.log(`${name} was Clicked`)
+    }
+    const handleClick3 = (e) => {
+        console.log(e.target.innerText)
+    }
 
   return (
    <main>
-        <p>
+        <p onDoubleClick={handleClick}>
             Hello {handleNameChange()}!
         </p>
+        <button onClick={handleClick}>Click it</button>
+        <button onClick={() => handleClick2('Dave')}>Click name</button>
+        <button onClick={(e) => handleClick3(e)}>Click this</button>
    </main>
   )
 }
